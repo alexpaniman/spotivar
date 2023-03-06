a87e0bcf5a0e1cfee80f42cd6113bf8b3698983b
alexxRT <alekseev.aa@phystech.edu>
2023-03-06 20:36:10 +0300

    messages read/write added

--- lib/server/net_connection.h
+++ lib/server/net_connection.h
@@ -34,20 +34,179 @@ namespace net
                 if (OwnerType == owner::server)
                 {
                     if (m_socket.is_open())
+                    {
                         id = uid;
+                        ReadHeader();
+                    }
                 }
-            }
+            };
 
-            bool ConnectionToServer(){};
-            bool Disconnect(){};
+            bool ConnectToServer(const boost::asio::ip::tcp::resolver::results_type &endpoints)
+            {
+                if (OwnerType == owner::client)
+                {
+                    boost::asio::async_connect(m_socket, endpoints,
+                    [this](std::error_code ec, boost::asio::ip::tcp::endpoint endpoint)
+                    {
+                        if (!ec)
+                        {
+                            ReadHeader();
+                        }
+                        else
+                        {
+                            std::cout << "Problem occured while connecting " << ec.message() << "\n";
+                        }
+                    }
+                    );
 
-            bool IsConnect() const
+                }
+            };
+
+            bool Disconnect()
+            {
+                if (IsConnected())
+                    boost::asio::post (asio_context, [this](){ m_socket.close(); });
+            };
+
+            bool IsConnected() const
             {
                 return m_socket.is_open();
             };
 
         public:
-            bool Send (const message<T>& msg) {};
+            bool Send (const message<T>& msg) 
+            {
+                boost::asio::post (asio_context, 
+                [this, msg]()
+                {
+                    bool WritingMessage = !qMessagesOut.empty();
+                    qMessagesOut.push_front(msg);
+
+                    if (!WritingMessage) //no messages to write
+                    {
+                        WriteHeader();
+                    }
+                }); 
+            };
+
+
+        protected:
+            //asynchronasly read message header
+            void ReadHeader()
+            {
+                boost::asio::async_read (m_socket, boost::asio::buffer(&TempMessage.header, sizeof(message_header<T>)),
+                [this](std::error_code ec, std::size_t length)
+                {
+                    if (!ec)
+                    {
+                            if (TempMessage.header.size > 0)
+                            {
+                                TempMessage.body.resize(TempMessage.header.size - sizeof(message_header<T>));
+                                ReadBody();
+                            }
+                            else
+                            {
+                                AddToIncommingQueue();
+                            }
+
+                    }
+                    else 
+                    {
+                        std::cout << "[" << id << "] " << "Failed to read header\n";
+                        m_socket.close();
+                    }
+                });
+
+            };
+
+            void ReadBody()
+            {
+                boost::asio::async_read  (m_socket, boost::asio::buffer(TempMessage.body.data(), TempMessage.body.size()),
+                [this](std::error_code ec, std::size_t length)
+                {
+                    if (!ec)
+                    {
+                        AddToIncommingQueue();
+                    }
+                    else 
+                    {
+                        //as above
+                        std::cout << "[" << id  << "] " << "Failed to read body\n";
+                        m_socket.close();
+                    }
+                }
+                ); 
+
+            };
+
+            void WriteHeader()
+            {
+                boost::asio::async_write(m_socket, boost::asio::buffer(&qMessagesOut.front().header, sizeof(message_header<T>)),
+                [this](std::error_code ec, std::size_t length)
+                {
+                    if (!ec)
+                    {
+                        if (qMessagesOut.front().body.size() > 0)
+                        {
+                            WriteBody();
+                        }
+                        else 
+                        {
+                            qMessagesOut.pop_front();
+
+                            if (!qMessagesOut.empty())
+                            {
+                                WriteHeader();
+                            }
+                        }
+                    }
+                    else 
+                    {
+                        std::cout << "[ " << id << " ]" << "Write Header Failed\n";
+                        m_socket.close();
+                    }
+
+                });
+
+            };
+
+            void WriteBody()
+            {
+                boost::asio::async_write(m_socket, boost::asio::buffer(qMessagesOut.front().body.data(), qMessagesOut.front().body.size()),
+                [this](std::error_code ec, std::size_t length)
+                {
+                    if (!ec)
+                    {
+                        qMessagesOut.pop_front();
+
+                        if (!qMessagesOut.empty())
+                        {
+                            WriteHeader();
+                        }
+                    }
+                    else 
+                    {
+                        std::cout << "[ " << id << " ]" << "Write Body Failed\n";
+                        m_socket.close();
+                    }
+                });
+
+                 
+            };
+        
+
+            void AddToIncommingQueue()
+            {
+                if (OwnerType == owner::server)
+                    qMessagesIn.push_back({ this->shared_from_this(), TempMessage });
+                else
+                    qMessagesIn.push_back({ nullptr, TempMessage });
+                
+                //std::cout << "Message put in private queue\n";
+
+                ReadHeader();
+            };
+
 
         protected:
             boost::asio::ip::tcp::socket m_socket;
@@ -60,6 +219,8 @@ namespace net
             //this message queue consists messages that indicate 
             //who in particular sent this message, namely reference to "connection" 
             ts_queue<owned_message<T>>& qMessagesIn; //this is not connection's queue, client or server has it
+
+            message<T> TempMessage;
 
             owner OwnerType = owner::server;
             uint32_t id = 0;
