3de44096349ca7cb2c1ec8ecd7cba3030d2529e8
alexxRT <alekseev.aa@phystech.edu>
2023-03-16 17:01:51 +0300

    FEATURE: crossplatform sha256 support added

--- lib/sha256/src/sha256.cpp
+++ lib/sha256/src/sha256.cpp
@@ -1,8 +1,52 @@
 #include "sha256/sha256.h"
-
-#include <byteswap.h>
 #include <assert.h>
 #include <string.h>
+
+#ifdef _MSC_VER
+
+#include <stdlib.h>
+#define __bswap_32(x) _byteswap_ulong(x)
+#define __bswap_64(x) _byteswap_uint64(x)
+
+#elif defined(__APPLE__)
+
+// Mac OS X / Darwin features
+#include <libkern/OSByteOrder.h>
+#define __bswap_32(x) OSSwapInt32(x)
+#define __bswap_64(x) OSSwapInt64(x)
+
+#elif defined(__sun) || defined(sun)
+
+#include <sys/byteorder.h>
+#define __bswap_32(x) BSWAP_32(x)
+#define __bswap_64(x) BSWAP_64(x)
+
+#elif defined(__FreeBSD__)
+
+#include <sys/endian.h>
+#define __bswap_32(x) bswap32(x)
+#define __bswap_64(x) bswap64(x)
+
+#elif defined(__OpenBSD__)
+
+#include <sys/types.h>
+#define __bswap_32(x) swap32(x)
+#define __bswap_64(x) swap64(x)
+
+#elif defined(__NetBSD__)
+
+#include <sys/types.h>
+#include <machine/bswap.h>
+#if defined(__BSWAP_RENAME) && !defined(__bswap_32)
+#define __bswap_32(x) bswap32(x)
+#define __bswap_64(x) bswap64(x)
+#endif
+
+#else
+
+#include <byteswap.h>
+
+#endif
 
 static const size_t BITS_IN_BYTE = 8;
 
